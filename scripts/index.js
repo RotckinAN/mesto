f0b72c45c52d62d9431c75d8b0aa9dd042b1f689
Anatolii Rotckin <rokkker@yandex.ru>
2022-06-30 14:05:52 +0200

    feat: добавить логику заполнения формы попапа

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,32 @@
-console.log('');
+let popupElement = document.querySelector('.popup');
+let popupCloseButtonElement = popupElement.querySelector('.popup__close');
+let popupOpenButtonElement = document.querySelector('.profile__edit-button');
+let popupSaveButtonElement = popupElement.querySelector('.popup__save-button');
+
+let formElement = document.querySelector('.popup__content');
+let nameInput = formElement.querySelector('.popup__nameInput');
+let jobInput = formElement.querySelector('.popup__jobInput');
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
+
+const openPopup = function() {
+    popupElement.classList.add('popup_opened');
+    nameInput.value = profileTitle.textContent;
+    jobInput.value = profileSubtitle.textContent;
+};
+
+const closePopup = function() {
+    popupElement.classList.remove('popup_opened');
+};
+
+popupOpenButtonElement.addEventListener('click', openPopup);
+popupCloseButtonElement.addEventListener('click', closePopup);
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    profileTitle.textContent = nameInput.value;
+    profileSubtitle.textContent = jobInput.value;
+}
+
+popupSaveButtonElement.addEventListener('click', closePopup);
+formElement.addEventListener('submit', formSubmitHandler);
